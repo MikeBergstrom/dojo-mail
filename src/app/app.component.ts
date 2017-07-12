6eb7b7f826f8f71df36daf6f867ae15e5d0e0b29
Mike Bergstrom <mikedberg@hotmail.com>
2017-07-12 08:51:49 -0700

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
-}
+  emails = [
+    {email: 'Bill@gates.com', important: true, subject: 'Job Offer', content: 'Want to pay million/year'},
+    {email: 'Info@codingdojo.com', important: true, subject: 'Class schedule', content: 'no class today'},
+    {email: 'John@Carmac.com', important: false, subject: 'Update Algo', content: 'New alogorithm for shadow volumes'},
+    {email: 'Gabe@Newel.com', important: false, subject: 'HL3', content: 'Just Kidding'}
+  ]
+};
+
